c525d739a7aacbca97683127e1216e7a9833e527
Vadym Kozachuk <77024298+KVadym-del@users.noreply.github.com>
2025-03-29 15:17:29 +0100

    Add project files.

---
+++ Bank/Program.cs
@@ -0,0 +1,32 @@
+﻿using Bank.Utilities;
+
+public class Account
+{
+    private double Balance = 0.0;
+
+    public void DepositFunds(double amount)
+    {
+        Balance += WalletUtils.IsAPositive(amount) ? amount : throw new ArgumentException("Depositing value should be positive.");
+    }
+
+    public void WithdrawFunds(double amount)
+    {
+        Balance -= WalletUtils.IsAPositive(amount) ? Balance > amount ? amount : throw new ArgumentException("Not enough money") : throw new ArgumentException("Withdraw value should be positive.");
+    }
+
+    public void GetBalance()
+    {
+        Console.WriteLine(Balance);
+    }
+
+    public static void Main(string[] args)
+    {
+        Account myAccount = new Account();
+
+        myAccount.DepositFunds(100.0);
+        myAccount.GetBalance();
+
+        myAccount.WithdrawFunds(50.0);
+        myAccount.GetBalance();
+    }
+}
---
+++ Bank/Utilities/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Utilities
+{
+    public static class WalletUtils
+    {
+        public static bool IsAPositive(double value) => value > 0;
+
+
+    }
+}
